148a78903ee4734e460871d88553e0a260ce224f
Alfro <arodriguez@teltek.es>
2015-11-26 11:11:45 +0100

    #8736 pumoodle/mod: Created new module. (WIP)

---
+++ Resources/data/pumoodle/mod/pumukit_url/lang/es/pumukit.php
@@ -0,0 +1,50 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for pumukit
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage pumukit
+ * @copyright  2012 Andres Perez
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Clase grabada';
+$string['modulenameplural'] = 'Clases grabadas';
+$string['modulename_help'] = 'El módulo pumukit permite incrustar un vídeo servido por un servidor pumukit como nuevo recurso';
+$string['pumukitfieldset'] = 'Custom example fieldset';
+$string['pumukitname'] = 'Nombre del vídeo en el curso';
+$string['pumukitname_'] = 'Nombre del vídeo en el curso';
+$string['pumukitname_help'] = 'Establece el nombre de este recurso dentro del curso';
+$string['pumukit'] = 'Pumukit';
+$string['pluginadministration'] = 'pumukit administration';
+$string['pluginname'] = 'PuMuKit';
+$string['select_a_video'] = 'Seleccione un vídeo';
+$string['error_no_pumukit_output'] = 'Error en la respuesta del servidor Pumukit';
+$string['form_rule_select_a_lecture'] = 'Debe seleccionar una clase grabada';
+$string['description'] = 'Descripción opcional para la clase grabada';
+$string['pumukiturl'] = 'URL del servidor Pumukit';
+$string['configpumukiturl'] = 'Moodle se conectará a este endpoint para consultar al servidor Pumukit';
+$string['pumukitsecret'] = 'Secreto compartido con Pumukit';
+$string['configpumukitsecret'] = 'Moodle usará este secreto compartido para cifrar las consultas al servidor Pumukit';
