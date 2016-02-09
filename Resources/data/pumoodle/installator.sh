dd3178392f1055ba4b140058c5f43751f420b714
Alfro <arodriguez@teltek.es>
2016-02-09 16:23:31 +0100

    Added 'installator' command.
    This command is a helper to quickly create the 'instal' files needed to install the modules/filter/repository on Moodle

---
+++ Resources/data/pumoodle/installator.sh
@@ -0,0 +1,7 @@
+cd mod
+zip -r ../install/modurl.zip pmkurlvideos
+zip -r ../install/modpersonal.zip pmkpersonalvideos
+cd ../repository
+zip -r ../install/repository.zip pumukit/
+cd ../filter
+zip -r ../install/filter.zip pumukit/
