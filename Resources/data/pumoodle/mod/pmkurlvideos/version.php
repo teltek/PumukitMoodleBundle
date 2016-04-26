8579c22a29185370dd916c9bb4a46c145b33eece
Alfonso Rodriguez <arodriguez@teltek.es>
2016-04-26 13:33:26 +0000

    #10422 mod: Fixed 'version.php' file to be 3.0 Compatible.

--- Resources/data/pumoodle/mod/pmkurlvideos/version.php
+++ Resources/data/pumoodle/mod/pmkurlvideos/version.php
@@ -31,8 +31,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 0;             // If version == 0 then module will not be installed
-$module->version   = 2016020201;    // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011051000;    // Requires this Moodle version
-$module->cron      = 0;             // Period for cron to check this module (secs)
-$module->component = 'mod_pmkurlvideos'; // To check on upgrade, that module sits in correct place
+$plugin->version   = 0;             // If version == 0 then module will not be installed
+$plugin->version   = 2016042600;    // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011051000;    // Requires this Moodle version
+$plugin->component = 'mod_pmkurlvideos'; // To check on upgrade, that module sits in correct place
