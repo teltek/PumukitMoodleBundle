805cc128ee9aadd20d45a9eeafc5e932696da4b2
Alfro <arodriguez@teltek.es>
2016-04-28 18:44:11 +0200

    pmkpersonalvideos: Fixed view to render an iframe.
    Now, instead of echoing the 'curl', there will be an iframe with the url and ticket

--- Resources/data/pumoodle/mod/pmkpersonalvideos/view.php
+++ Resources/data/pumoodle/mod/pmkpersonalvideos/view.php
@@ -29,7 +29,7 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
-require_once(dirname(__FILE__).'/locallib.php'); 
+require_once(dirname(__FILE__).'/locallib.php');
 
 global $USER;
 
@@ -37,13 +37,13 @@ $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
 $n  = optional_param('n', 0, PARAM_INT);  // pmkpersonalvideos instance ID - it should be named as the first character of the module
 
 if ($id) {
-	$cm      = get_coursemodule_from_id('pmkpersonalvideos', $id, 0, false, MUST_EXIST);
-	$course  = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-	$pmkpersonalvideos = $DB->get_record('pmkpersonalvideos', array('id' => $cm->instance), '*', MUST_EXIST);
+    $cm      = get_coursemodule_from_id('pmkpersonalvideos', $id, 0, false, MUST_EXIST);
+    $course  = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $pmkpersonalvideos = $DB->get_record('pmkpersonalvideos', array('id' => $cm->instance), '*', MUST_EXIST);
 } elseif ($n) {
-	$pmkpersonalvideos = $DB->get_record('pmkpersonalvideos', array('id' => $n), '*', MUST_EXIST);
-	$course  = $DB->get_record('course', array('id' => $pmkpersonalvideos->course), '*', MUST_EXIST);
-	$cm      = get_coursemodule_from_instance('pmkpersonalvideos', $pmkpersonalvideos->id, $course->id, false, MUST_EXIST);
+    $pmkpersonalvideos = $DB->get_record('pmkpersonalvideos', array('id' => $n), '*', MUST_EXIST);
+    $course  = $DB->get_record('course', array('id' => $pmkpersonalvideos->course), '*', MUST_EXIST);
+    $cm      = get_coursemodule_from_instance('pmkpersonalvideos', $pmkpersonalvideos->id, $course->id, false, MUST_EXIST);
 } else {
     error('You must specify a course_module ID or an instance ID');
 }
@@ -53,9 +53,9 @@ $context = context_module::instance($cm->id);
 
 //New 'all view' log event.
 $event = \mod_pmkpersonalvideos\event\course_module_viewed::create(array(
-								  'objectid' => $cm->instance,
-								  'context' => $context,
-								  ));
+    'objectid' => $cm->instance,
+    'context' => $context,
+));
 $event->add_record_snapshot('course', $course);
 $event->trigger();
 // Update 'viewed' state if required by completion system
@@ -63,13 +63,11 @@ require_once($CFG->libdir . '/completionlib.php');
 $completion = new completion_info($course);
 $completion->set_module_viewed($cm);
 
-
 /// Print the page header
 $PAGE->set_url('/mod/pmkpersonalvideos/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($pmkpersonalvideos->name));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_context($context);
-
 // other things you may want to set - remove if not needed
 //$PAGE->set_cacheable(false);
 //$PAGE->set_focuscontrol('some-html-id');
@@ -77,30 +75,36 @@ $PAGE->set_context($context);
 
 // Output starts here
 echo $OUTPUT->header();
-
 echo $OUTPUT->heading($pmkpersonalvideos->name);
 
 if ($pmkpersonalvideos->intro) { // Conditions to show the intro can change to look for own settings or whatever
     echo $OUTPUT->box(format_module_intro('pmkpersonalvideos', $pmkpersonalvideos, $cm->id), 'generalbox mod_introbox', 'pmkpersonalvideosintro');
 }
 
-// Creates a ticket to view the file to ensure that the video is displayed.
-// as Pmkpersonalvideos requires it for the non-public broadcast profiles.
-$url_temp_array = explode('/', $pmkpersonalvideos->embed_url); // Strict standards: Only variables should be passed by reference
-
-
-preg_match('/id\=(\w*)\&|id\=(\w*)$/i', $pmkpersonalvideos->embed_url, $result);
-$mm_id = isset($result[1])?  $result[1] : null;
-preg_match('/lang\=(\w*)\&|id\=(\w*)$/i', $pmkpersonalvideos->embed_url, $result);
-$lang = isset($result[1])?  $result[1] : null;
+$link_params = array();
+parse_str(html_entity_decode(parse_url($pmkpersonalvideos->embed_url, PHP_URL_QUERY)), $link_params);
+$mm_id = isset($link_params['id']) ? $link_params['id'] : null;
+$lang = isset($link_params['lang']) ? $link_params['lang'] : null;
+$opencast = isset($link_params['opencast']) ? ($link_params['opencast'] == '1') : false;
 $concatChar = ($mm_id || $lang) ? '&': '?';
 $parameters = array(
-              'professor_email' => $pmkpersonalvideos->professor_email,
-              'ticket' => pmkpersonalvideos_create_ticket($mm_id, $pmkpersonalvideos->professor_email)
-              );
+    'professor_email' => $pmkpersonalvideos->professor_email,
+    'ticket' => pmkpersonalvideos_create_ticket($mm_id, $pmkpersonalvideos->professor_email)
+);
 $url = $pmkpersonalvideos->embed_url . $concatChar . http_build_query($parameters, '', '&');
-// TO DO - test an iframe instead of direct curl echo.
-echo pmkpersonalvideos_curl_action_parameters($url , null, true);
 
+if($opencast) {
+    $iframe_width = '100%';
+    $iframe_height = '600px' ;
+}
+else {
+    $iframe_width = '600px';
+    $iframe_height = '400px' ;
+}
+$iframe_html = '<iframe src="' . $url . '"' .
+               '        style="border:0px #FFFFFF none; width:' . $iframe_width . '; height:' . $iframe_height . ';"' .
+               '        scrolling="no" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" >'.
+               '</iframe>';
+echo $iframe_html;
 // Finish the page
 echo $OUTPUT->footer();
