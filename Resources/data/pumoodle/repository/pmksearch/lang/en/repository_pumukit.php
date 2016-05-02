d536305509a0117a728670a28b33a41514a43063
Alfro <arodriguez@teltek.es>
2016-05-02 17:41:58 +0200

    #10442 Added new 'pmksearch' repository.
    * Copied the 'pumukit' repository.

---
+++ Resources/data/pumoodle/repository/pmksearch/lang/en/repository_pumukit.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains pumukit repository plugin language strings
+ */
+$string['pluginname_help'] = 'Pumukit video repository';
+$string['pluginname'] = 'PuMuKit videos';
+$string['pumukit:view'] = 'View pumukit repository';
+$string['configplugin'] = 'Pumukit repository configuration';
+$string['account'] = 'Account information';
+$string['configplugin'] = 'Configuration for this plugin';
+$string['api_key'] = 'API Key';
+$string['error_no_pumukit_output'] = 'Error - Pumukit server response error';
+$string['pumukiturl'] = 'Pumukit server URL';
+$string['pumukitsecret'] = 'Pumukit shared secret';
+$string['pumukiturldefault'] = 'Default server URL: ';
+$string['pumukitsecretdefault'] = 'Default pumukit secret: ';
+
