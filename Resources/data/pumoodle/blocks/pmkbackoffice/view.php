92279044af9991edead814b7ed23184dadf3b862
Alfro <arodriguez@teltek.es>
2016-05-24 16:42:31 +0200

    #10609 blocks/pmkbackoffice: Added correct pmk2 manager iframe.
    * Added 'askForHeight' logic, using javascript post/receiveMessage to communicate with the iframe for dynamic height changes.
    * Added logic to receive the 'url' for the iframe through GET request. (fix)

--- Resources/data/pumoodle/blocks/pmkbackoffice/view.php
+++ Resources/data/pumoodle/blocks/pmkbackoffice/view.php
@@ -7,7 +7,7 @@ global $DB, $OUTPUT, $PAGE;
 
 // Check for all required variables.
 $courseid = required_param('courseid', PARAM_INT);
-
+$pmk2_url = required_param('url', PARAM_RAW);
 
 if (!$course = $DB->get_record('course', array('id' => $courseid))) {
     print_error('invalidcourse', 'block_pmkbackoffice', $courseid);
@@ -20,12 +20,30 @@ $PAGE->set_heading(get_string('pagetitle', 'block_pmkbackoffice'));
 
 echo $OUTPUT->header();
 ?>
-<iframe src="https://pumukit2.armazeg.org/app_dev.php/admin"
+<script>
+ function requestIframeHeight(ifrm) {
+     ifrm.contentWindow.postMessage("gimme_iframe_height", "*");
+ }
+ window.addEventListener("message", setNewHeight, false);
+ function setNewHeight(event){
+     ifrm = document.getElementById('pmk_iframe');
+     if(event.data.height) {
+         ifrm.style.visibility = 'hidden';
+         ifrm.style.height = "10px"; // reset to minimal height ...
+         // IE opt. for bing/msn needs a bit added or scrollbar appears
+         ifrm.style.height = event.data.height + 4 + "px";
+         ifrm.style.visibility = 'visible';
+     }
+ }
+</script>
+<iframe src="<?php echo $pmk2_url ?>"
+        id='pmk_iframe'
         width="100%"
-        height="100%"
+        height="2200px"
+        onload="requestIframeHeight(this)"
+        scrolling="no"
         border="0"
         frameborder="0">
-
 </iframe>
 <?php
 echo $OUTPUT->footer();
