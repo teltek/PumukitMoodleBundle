77a23bf1fa7231b4a823673dda64613c81fdeb4e
Alfro <arodriguez@teltek.es>
2016-05-24 17:39:26 +0200

    #10609 blocks/pmkbackoffice: Added new strings and translated to spanish.

--- Resources/data/pumoodle/blocks/pmkbackoffice/lang/en/block_pmkbackoffice.php
+++ Resources/data/pumoodle/blocks/pmkbackoffice/lang/en/block_pmkbackoffice.php
@@ -1,7 +1,8 @@
 <?php
-$string['linktopage'] = 'Go to PuMuKIT Media Manager';
-$string['pagetitle'] = 'PuMuKIT Media Manager';
-$string['pluginname'] = 'PuMuKIT Back-Office block';
-$string['pmkbackoffice'] = 'PuMuKIT Back-Office';
-$string['pmkbackoffice:addinstance'] = 'Add a new PuMuKIT Back-Office block';
-$string['pmkbackoffice:myaddinstance'] = 'Add a new PuMuKIT Back-Office to the My Moodle page';
+$string['linktopage'] = 'Go to PuMuKIT - Media Manager';
+$string['pagetitle'] = 'PuMuKIT - Media Manager';
+$string['pagenotfoundtext'] = 'The \'pmksearch\' repository instance was not found. Maybe the wrong id was added?';
+$string['pluginname'] = 'PuMuKIT - Media Manager block';
+$string['pmkbackoffice'] = 'PuMuKIT - Media Manager';
+$string['pmkbackoffice:addinstance'] = 'Add a new PuMuKIT - Media Manager block';
+$string['pmkbackoffice:myaddinstance'] = 'Add a new PuMuKIT - Media Manager to the My Moodle page';
---
+++ Resources/data/pumoodle/blocks/pmkbackoffice/lang/es/block_pmkbackoffice.php
@@ -0,0 +1,8 @@
+<?php
+$string['linktopage'] = 'Ir al Media Manager de PuMuKIT';
+$string['pagetitle'] = 'PuMuKIT - Media Manager';
+$string['pagenotfoundtext'] = 'La instancia del repositorio \'pmksearch\' no fue encontrada. ¿Quizás se envió el id equivocado?';
+$string['pluginname'] = 'PuMuKIT - Media Manager block';
+$string['pmkbackoffice'] = 'PuMuKIT - Media Manager';
+$string['pmkbackoffice:addinstance'] = 'Añadir un nuevo \'block\' PuMuKIT - Media Manager';
+$string['pmkbackoffice:myaddinstance'] = 'Añadir un nuevo PuMuKIT - Media Manager a mi página de Moodle';
