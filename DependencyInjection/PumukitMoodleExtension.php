1ae9fd2a42e501bae74b511204243368c106d4b2
rubenrua <rubenrua@teltek.es>
2016-06-03 14:19:27 +0200

    Fix bug with order loading the parameters

--- DependencyInjection/PumukitMoodleExtension.php
+++ DependencyInjection/PumukitMoodleExtension.php
@@ -2,28 +2,39 @@
 
 namespace Pumukit\MoodleBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class PumukitMoodleExtension extends Extension
+class PumukitMoodleExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        //Necessary to use the parameters in PumukitNewAdminBundle
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
 
         $container->setParameter('pumukit_moodle.password', $config['password']);
         $container->setParameter('pumukit_moodle.role', $config['role']);
@@ -36,4 +47,5 @@ class PumukitMoodleExtension extends Extension
             $container->setParameter('pumukit2.naked_backoffice_background', $config['naked_backoffice_background']);
         }
     }
+
 }
