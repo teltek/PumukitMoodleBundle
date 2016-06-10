3fe9d7d7bca6a2e199c6f99a1682185eecf1c5dc
Alfro <arodriguez@teltek.es>
2016-06-10 11:36:46 +0200

    Added zip installers for new repositories.

--- Resources/data/pumoodle/installator.sh
+++ Resources/data/pumoodle/installator.sh
@@ -3,5 +3,8 @@ zip -r ../install/modurl.zip pmkurlvideos
 zip -r ../install/modpersonal.zip pmkpersonalvideos
 cd ../repository
 zip -r ../install/repository.zip pumukit/
+zip -r ../install/pmksearch.zip pmksearch/
 cd ../filter
 zip -r ../install/filter.zip pumukit/
+cd ../blocks
+zip -r ../install/pmkbackoffice.zip pmkbackoffice/
