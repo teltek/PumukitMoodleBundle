887f9e13217f668e6a49595310310acfb2591418
Alfro <arodriguez@teltek.es>
2016-06-13 09:39:59 +0200

    block: Fixed installation error. Removed get_content.
    The block would not install unless applicable_formats returns an array with a field set to true.

--- Resources/data/pumoodle/blocks/pmkbackoffice/block_pmkbackoffice.php
+++ Resources/data/pumoodle/blocks/pmkbackoffice/block_pmkbackoffice.php
@@ -3,20 +3,11 @@ class block_pmkbackoffice extends block_base {
     public function init() {
         $this->title = get_string('pmkbackoffice', 'block_pmkbackoffice');
     }
-    public function get_content() {
-        global $COURSE;
-
-        if ($this->content !== null) {
-            return $this->content;
-        }
-
-        $url = new moodle_url('/blocks/pmkbackoffice/view.php', array('blockid' => $this->instance->id, 'courseid' => $COURSE->id));
-        $this->content         =  new stdClass;
-        $this->content->footer = html_writer::link($url, sprintf('--> %s <--',get_string('linktopage', 'block_pmkbackoffice')));
-        return $this->content;
-    }
-    //Prevents the block from appearing anywhere.
+    //Prevents the block from appearing anywhere. (since we only use it to have the back-office show into an iframe.)
     public function applicable_formats() {
-        return array();
+        return array(
+            'all' => false,
+            'nowhere' => true, //WORKAROUND: If the returned array does not contain something set to 'true', the plugin cannot be installed.
+        );
     }
 }
