989e870686668d8a4fa1217d4e1d88969374ff25
rubenrua <rubenrua@teltek.es>
2016-07-14 17:28:33 +0200

    Create User if not exists

--- Controller/SSOController.php
+++ Controller/SSOController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Pumukit\SchemaBundle\Services\UserService;
+use Pumukit\SchemaBundle\Document\User;
+use Pumukit\SchemaBundle\Document\Group;
 
 /**
  * @Route("/pumoodle")
@@ -23,7 +26,6 @@ class SSOController extends Controller
      */
     public function ssoAction(Request $request)
     {
-
         //TODO Disable by default
         if (!$this->container->hasParameter('pumukit2.naked_backoffice_domain')) {
             throw $this->createNotFoundException('Naked backoffice domain not conf');
@@ -60,7 +62,11 @@ class SSOController extends Controller
         //Find User
         $user = $repo->findOneBy(array('email' => $email));
         if (!$user) {
-            throw $this->createNotFoundException('user not found!');
+            try {
+                $this->createUser($email);
+            } catch (\Exception $e) {
+                throw $this->createNotFoundException($e->getMessage());
+            }
         }
 
         /*
@@ -87,5 +93,65 @@ class SSOController extends Controller
         $this->get('security.token_storage')->setToken($token);
         $event = new InteractiveLoginEvent($request, $token);
         $this->get('event_dispatcher')->dispatch('security.interactive_login', $event);
+    }
+
+    private function createUser($email)
+    {
+        $ldapSerive = $this->get('pumukit_ldap.ldap');
+        $permissionProfileService = $this->get('pumukitschema.permissionprofile');
+        $userService = $this->container->get('pumukitschema.user');
+        $personService = $this->container->get('pumukitschema.person');
+
+        $info = $ldapSerive->getInfoFromEmail($email);
+
+        if (!$info) {
+            throw new \RuntimeException('user not found!');
+        }
+        //TODO Move to a service
+        // isset($info["eduPersonPrimaryAffiliation"][0]) && in_array($info["eduPersonPrimaryAffiliation"][0], array('PAS', 'PDI'))
+        if (true) {
+            throw new \RuntimeException('user not valid');
+        }
+
+        //TODO create createDefaultUser in UserService.
+        //$this->userService->createDefaultUser($user);
+        $user = new User();
+        $user->setUsername($info["cn"][0]);
+        $user->setEmail($info["mail"][0]);
+
+        $permissionProfile = $permissionProfileService->getByName("Auto Publisher");
+        $user->setPermissionProfile($permissionProfile);
+        $user->setOrigin('moodle');
+        $user->setEnabled(true);
+
+        $userService->create($user);
+        $group = $this->getGroup($info["eduPersonPrimaryAffiliation"][0]);
+        $userService->addGroup($group, $user, true, false);
+        $personService->referencePersonIntoUser($user);
+
+        return $user;
+    }
+
+    private function getGroup($key)
+    {
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+        $repo = $dm->getRepository('PumukitSchemaBundle:Group');
+        $groupService = $this->get('pumukitschema.group');
+
+        $cleanKey = preg_replace('/\W/', '', $key);
+
+        $group = $repo->findOneByKey($cleanKey);
+        if ($group) {
+            return $group;
+        }
+
+        $group = new Group();
+        $group->setKey($cleanKey);
+        $group->setName($key);
+        $group->setOrigin('cas');
+        $groupService->create($group);
+
+        return $group;
+
     }
 }
