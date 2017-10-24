31867d9b07de89d2c46924ad1558b7ce87fcf08f
rubenrua <rubenrua@teltek.es>
2017-10-24 14:16:59 +0200

    Add BasicSearchController::createMultimediaObjectQueryBuilder

--- Controller/BasicSearchController.php
+++ Controller/BasicSearchController.php
@@ -27,4 +27,22 @@ class BasicSearchController extends SearchController
     {
         return parent::multimediaObjectsAction($request, $blockedTag, $useTagAsGeneral);
     }
+
+    protected function createMultimediaObjectQueryBuilder()
+    {
+        $repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $request = $this->get('request_stack')->getMasterRequest();
+
+        if ('/pumoodle/searchmultimediaobjects' == $request->getPathInfo()) {
+            $dm = $this->get('doctrine_mongodb.odm.document_manager');
+            $dm->getFilterCollection()->disable('frontend');
+            $queryBuilder = $repo->createQueryBuilder();
+            $queryBuilder->field('status')->equals(0);
+            $queryBuilder->field('properties.redirect')->equals(false);
+        } else {
+            $queryBuilder = $repo->createStandardQueryBuilder();
+        }
+
+        return $queryBuilder;
+    }    
 }
